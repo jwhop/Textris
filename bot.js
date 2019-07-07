f4ccd8588bcea50505d006b9ee9b56caa3a0bbdc
Jonathan Hopkins <jwhopkins@ucdavis.edu>
2019-07-07 14:12:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -232,7 +232,7 @@ client.on('message', message => {
 				else
 					msg_2 += ('\t' + "           ");
 				for(l = 0; l < 4; l++){
-					held_piece = tg.game.gethold_piece();
+					held_piece = tg.game.get_hold_piece();
 					if(held_piece == ' ')
 					{
 						msg_2 += ":black_circle:";
