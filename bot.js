fa1b8baf965824ebcda76f8f14f83f7a58750061
Jonny Hopkins <jwhopkins@ucdavis.edu>
2020-02-21 13:22:13 -0500

    try to fix custom emoji

--- bot.js
+++ bot.js
@@ -346,7 +346,8 @@ function send_board_message(tg) {
 		for (j = 0; j < 10; j++){
 			let e = tg.game.board[i][j];
 			if(tg.game.alt_emojis[e.substr(1, e.length - 2)]  != undefined){
-				msg += ':' + tg.game.alt_emojis[e.substr(1, e.length - 2)] + ':';
+				const ayy = message.guild.emojis.find(emoji => emoji.name === "ayy");
+				msg += ${ayy};
 			}
 			else{
 				msg += e;
